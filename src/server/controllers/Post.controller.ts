f1841643eabab8f2b198eb85e6ca3dba43239b7c
Orim Dominic <orimdominic@gmail.com>
2021-05-27 18:56:48 +0100

    feat: delete single post

--- src/server/controllers/Post.controller.ts
+++ src/server/controllers/Post.controller.ts
@@ -5,48 +5,9 @@ import { PostModel } from "../models";
 // TODO: Set response data to follow a format
 // TODO: Write docs for controller fns
 // TODO: Write tests
+// TODO: Use AppHttpError
 
 export class PostController {
-  static createPost: RequestHandler = async (req, res) => {
-    // TODO: User should be able to upload images
-    const { post } = req.body;
-    const { timestamp } = post;
-    delete post.timestamp;
-    try {
-      const postDoc = await PostModel.create({
-        ...post,
-        // TODO: Get author name from user in db
-        author: "author",
-        createdAt: timestamp,
-        updatedAt: timestamp,
-      });
-      res.setHeader("Location", `/posts/${postDoc.id}`);
-      res.status(StatusCodes.CREATED).json(postDoc);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send(JSON.stringify(err, null, 2));
-    }
-  };
-
-  static updatePost: RequestHandler = async (req, res) => {
-    // TODO: User should be able to upload images
-    const { id } = req.params;
-    const { post } = req.body;
-    try {
-      const postDoc = await PostModel.findById({ _id: id });
-      if (!postDoc) {
-        return res.status(404).send("Not found");
-      }
-      postDoc.updatedAt = post.timestamp;
-      postDoc.content = post.content;
-      const newPostDoc = await postDoc.save();
-      res.status(StatusCodes.CREATED).send(JSON.stringify(newPostDoc, null, 2));
-    } catch (err) {
-      console.error(err);
-      res.status(500).send(JSON.stringify(err, null, 2));
-    }
-  };
-
   static getOnePost: RequestHandler = async (req, res) => {
     const { id } = req.params;
 
@@ -73,6 +34,61 @@ export class PostController {
         return res.status(StatusCodes.OK).json([]);
       }
       return res.status(StatusCodes.OK).json(postDocs);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(JSON.stringify(err, null, 2));
+    }
+  };
+
+  static updateOnePost: RequestHandler = async (req, res) => {
+    // TODO: User should be able to upload images
+    const { id } = req.params;
+    const { post } = req.body;
+    try {
+      const postDoc = await PostModel.findById({ _id: id });
+      if (!postDoc) {
+        return res.status(404).send("Not found");
+      }
+      postDoc.updatedAt = post.timestamp;
+      postDoc.content = post.content;
+      const newPostDoc = await postDoc.save();
+      res.status(StatusCodes.CREATED).json(newPostDoc);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(JSON.stringify(err, null, 2));
+    }
+  };
+
+  static createOnePost: RequestHandler = async (req, res) => {
+    // TODO: User should be able to upload images
+    const { post } = req.body;
+    const { timestamp } = post;
+    delete post.timestamp;
+    try {
+      const postDoc = await PostModel.create({
+        ...post,
+        // TODO: Get author name from user in db
+        author: "author",
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      });
+      res.setHeader("Location", `/posts/${postDoc.id}`);
+      res.status(StatusCodes.CREATED).json(postDoc);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(JSON.stringify(err, null, 2));
+    }
+  };
+
+  static deleteOnePost: RequestHandler = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const postDoc = await PostModel.findById({ _id: id });
+      if (!postDoc) {
+        return res.status(404).send("Not found");
+      }
+      await PostModel.findByIdAndDelete(id);
+      res.status(StatusCodes.NO_CONTENT).send();
     } catch (err) {
       console.error(err);
       res.status(500).send(JSON.stringify(err, null, 2));
