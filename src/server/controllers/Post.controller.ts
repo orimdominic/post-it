7b93f6b00ea3dbc3d0c2dc173d39e9bfa97d077c
Orim Dominic Adah <orimdominic@gmail.com>
2021-05-27 17:33:51 +0100

    feat: get single post (#4)

--- src/server/controllers/Post.controller.ts
+++ src/server/controllers/Post.controller.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+import { StatusCodes, getReasonPhrase } from "http-status-codes";
 import { PostModel } from "../models";
 
 export class PostController {
@@ -14,7 +15,25 @@ export class PostController {
         updatedAt: timestamp,
       });
       res.setHeader("Location", `/posts/${newPost.id}`);
-      res.status(200).send(JSON.stringify(newPost, null, 2));
+      res.status(StatusCodes.CREATED).send(JSON.stringify(newPost, null, 2));
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(JSON.stringify(err, null, 2));
+    }
+  };
+
+  static getPost: RequestHandler = async (req, res) => {
+    const { id } = req.params;
+    console.log("post id:", id);
+
+    try {
+      const post = await PostModel.findById({ _id: id });
+      if (!post) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .send(getReasonPhrase(StatusCodes.NOT_FOUND));
+      }
+      return res.status(StatusCodes.OK).json(post);
     } catch (err) {
       console.error(err);
       res.status(500).send(JSON.stringify(err, null, 2));
