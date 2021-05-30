7fd7197e1f87de28f833307b1eab5642d6805e1e
Orim Dominic <orimdominic@gmail.com>
2021-05-30 04:30:31 +0100

    tests: router

---
+++ src/server/routes/api/__tests__/auth.router.unit.test.ts
@@ -0,0 +1,26 @@
+import authRouter from "../auth.router";
+jest.useFakeTimers();
+
+describe("authRouter", () => {
+  it(`has the following routes
+- post /auth/register,
+- post /auth/login,
+- post /auth/logout,
+- post /auth/forgot-password,
+- patch /auth/password-reset,`, () => {
+    const routes = [
+      "post /auth/register",
+      "post /auth/login",
+      "post /auth/logout",
+      "post /auth/forgot-password",
+      "patch /auth/password-reset",
+    ];
+    for (const r of routes) {
+      const [method, path] = r.split(" ");
+      const match = authRouter.stack.find(
+        (s) => s.route.path === path && s.route.methods[method]
+      );
+      expect(match).toBeTruthy();
+    }
+  });
+});
